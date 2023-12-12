46ad7ad2d52236c83660060265942d5ab6f7cb19
Armoghan <Armoghan16@gmail.com>
2023-12-12 15:34:49 +0500

    Added Light/Dark Theme Colors and Font Family

--- assets/css/tailwind.config.js
+++ assets/css/tailwind.config.js
@@ -6,7 +6,33 @@ module.exports = {
         "./content/**/*.html"
     ],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                'primary': 'rgb(var(--primary))',
+                'secondary': 'rgb(var(--secondary))',
+                'accent': 'rgb(var(--accent))',
+                'dark-primary': 'rgb(var(--dark-primary))',
+                'dark-secondary': 'rgb(var(--dark-secondary))',
+                'dark-accent': 'rgb(var(--dark-accent))',
+            },
+            fontSize: {
+                sm: '0.750rem',
+                base: '1rem',
+                xl: '1.333rem',
+                '2xl': '1.777rem',
+                '3xl': '2.369rem',
+                '4xl': '3.158rem',
+                '5xl': '4.210rem',
+            },
+            fontFamily: {
+                heading: 'Poppins',
+                body: 'Happy Monkey',
+            },
+            fontWeight: {
+                normal: '400',
+                bold: '700',
+            }
+        },
     },
     plugins: [
         require('@tailwindcss/aspect-ratio'),
